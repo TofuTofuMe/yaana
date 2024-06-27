cddea05c0cb62fd4164dbf5b8d397072753b7378
TofuTofuMe <santoschristopheredison@protonmail.com>
2024-06-28 03:13:42 +0800

    Add anime list widget to the home screen

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,24 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:yaana/widgets/anime_list.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Center(
-        child: ListView(
-          children: <Widget>[
-            SizedBox(
-              height: 500,
-              child: ListView(
-                padding: EdgeInsets.fromLTRB(0.0, 32.0, 0.0, 16.0),
-                children: [],
-              ),
-            ),
-          ],
-        ),
-      ),
-    ));
+        body: Container(
+            margin: EdgeInsets.only(top: 16.0, bottom: 16.0),
+            child: Center(child: const AnimeListWidget())));
   }
 }
