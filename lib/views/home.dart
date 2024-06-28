cd65c37b967d3f6cafd47ed3c81504216affeeb6
TofuTofuMe <santoschristopheredison@protonmail.com>
2024-06-28 17:05:18 +0800

    Make Home into a ConsumerStatefulWidget

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yaana/widgets/anime_list.dart';
 
-class Home extends StatelessWidget {
+class Home extends ConsumerStatefulWidget {
+  const Home({super.key});
+
+  @override
+  ConsumerState<Home> createState() => _HomeState();
+}
+
+class _HomeState extends ConsumerState<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
