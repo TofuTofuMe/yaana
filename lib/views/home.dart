07128a003709ea21915c9a65687b0391d4b350f1
TofuTofuMe <santoschristopheredison@protonmail.com>
2024-06-28 17:07:24 +0800

    Show top-airing anime in the home screen

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yaana/data/client.dart';
+import 'package:yaana/providers/animelist_provider.dart';
+import 'package:yaana/providers/config_provider.dart';
 import 'package:yaana/widgets/anime_list.dart';
 
 class Home extends ConsumerStatefulWidget {
@@ -10,6 +13,15 @@ class Home extends ConsumerStatefulWidget {
 }
 
 class _HomeState extends ConsumerState<Home> {
+  @override
+  void initState() {
+    super.initState();
+    final animeService = ref.read(configProvider).animeService;
+    getTopAnime(animeService).then((animeList) {
+      ref.read(animeListProvider.notifier).setList(animeList);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
